827a48baedf7632559d03fef38c8ed8580c6bef0
destroyerpants <houstonbass96@gmail.com>
2015-06-17 14:48:40 -0400

    Dynamic table for tabs

--- Files/Tabs/index.php
+++ Files/Tabs/index.php
@@ -17,15 +17,29 @@
 					<h1>Houston's Tabs</h1>
 					<p>That toats are not stolen from <a href="http://tabs.ultimate-guitar.com/"> ultimate-guitar.com</a></p>
 				</div>
-				
-				<div class="cell" id="table">
+				<div class="cell" id="table" style="color:white">
 					<table>
-						<tr>
-							<th>Taylor Swift</th>
-						</tr>
-						<tr>
-							<td><a href="Taylor Swift/Red.html">Red</a></td>
-						</tr>
+						<?php
+							$dir =  '.';
+							$dirContent = scandir($dir);
+							
+							foreach ($dirContent as $cont) {
+								if ($cont === '.' or $cont === '..') continue;
+								if (is_dir($dir . '/' . $cont)) {
+									$htmlPages = scandir($dir . '/' . $cont);
+									print('<tr><th>' .  $cont . '</th></tr>');
+									if ($cont === '.' or $cont === '..') continue;
+									if (is_dir($dir . '/' . $cont)) {
+										$htmlPages = scandir($dir . '/' . $cont);
+										foreach($htmlPages as $page) {
+											if ($page === '.' or $page === '..') continue;
+											print('<tr><td><a href="' . $cont . '/' . $page . '">' . preg_replace('/\\.[^.\\s]{3,4}$/', '', $page) . "</a></td><tr>");
+										}
+									print('</tr>');
+									}
+								}
+							}
+						?>
 					</table>
 					
 				</div>
@@ -38,8 +52,8 @@
 		<div id="footer">
 			<div class="container">
 				<p class="muted credit">Created by <a href="">Houston Bass</a> using <a href="http://getbootstrap.com/">Bootstrap</a> and <a href="http://getbootstrap.com/examples/sticky-footer/">this</a> template.</p>
+				<a href="Taylor Swift/Red.html">Red</a>
 			</div>
 		</div>
 	<body>
-	
 </html>
